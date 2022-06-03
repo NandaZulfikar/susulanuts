4f555f04f83844ec7fc62ae31a7504c0942f5532
Vkar <101820378+NandaZulfikar@users.noreply.github.com>
2022-06-03 15:34:47 +0700

    uts

---
+++ 01dan02. # Type data non primitive.java
@@ -0,0 +1,11 @@
+# Type data non primitive
+Tipe Data referensi
+Tipe Data String
+Array Java
+class
+Interface
+Enum Java
+
+# Array adalah variabel yang mempunyai indeks sehingga dapat menyimpan sejumlah data yang bersifat sama. Dimensi array adalah jumlah indekspada variabel array. Array multi dimensi (lebih dari satu indeks,maksimal 7 indeks). Dalam perhitungan, Array sering digunakan untuk operasi matriks.
+
+#
---
+++ 03. jawabanNO3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MethodParameter {
+    private static void cetaknama(String nama1, String nama2){
+        system.out.println("nama1");
+        system.out.println("nama2");
+    }
+
+    public static void main(String[] args) {
+        cetaknama("jilly","kharisma");
+        Scanner a= new Scanner (system.in);
+
+    }
+    
+}
